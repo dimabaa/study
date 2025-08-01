d6002b539e29b5e1b6136c64c365f0c0b6318221
dimabaa <di.barbashin@gmail.com>
2025-08-01 13:22:38 +0300

    1

--- todo_list/main.go
+++ todo_list/main.go
@@ -1,11 +1,55 @@
 package main
 
 import (
-	"todo_list/scanner"
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/k0kubun/pp"
 )
 
+type TaskStruct struct {
+	Subject    string
+	Text       string
+	TimeCreate time.Time
+	Completed  bool
+}
+
+func Task(subject string, text string) TaskStruct {
+	return TaskStruct{
+		Subject:    subject,
+		Text:       text,
+		TimeCreate: time.Now(),
+		Completed:  false,
+	}
+
+}
 func main() {
 
-	scanner.PrintSS()
+	SliceTask := make([]int, 0, 10)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Вас приветствует ToDo List")
+	fmt.Println("Введите одну из команд:")
+	fmt.Println("			добавить")
+	fmt.Println("			удалить")
+	fmt.Println("			лист")
+	fmt.Print("Ждём ввод: ")
+
+	scanner.Scan()
+	vvod := scanner.Text()
+	if vvod == "добавить" {
+		fmt.Print("Введите тему задачи: ")
+		scanner.Scan()
+		vvodSubject := scanner.Text()
+		fmt.Print("Введите текст: ")
+		scanner.Scan()
+		vvodText := scanner.Text()
+		new := Task(vvodSubject, vvodText)
+		pp.Println(new)
+		SliceTask = append(SliceTask, new)
+	}
+	fmt.Println("Ваша команда:", vvod)
 
 }
